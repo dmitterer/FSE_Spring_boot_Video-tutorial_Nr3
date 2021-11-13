89446f96be27f2c564ee3629dd3f06fa2a15a85e
Dominik Mitterer <dmitterer@tsn.at>
2021-11-13 12:12:50 +0100

    Implementieren von der Methode "printAllStudent" in "StudentRepositoryTest" siehe Word Doku Unterpunkt 6.4.3

--- src/test/java/com/itkollegimst/spring/data/jpa/tutorial/repository/StudentRepositoryTest.java
+++ src/test/java/com/itkollegimst/spring/data/jpa/tutorial/repository/StudentRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -31,4 +33,15 @@ class StudentRepositoryTest {
 
     }
 
+    @Test
+    public void printAllStudent(){
+
+        List<Student> studentList =
+                studentRepository.findAll();
+
+
+        System.out.println("studentList" + studentList);
+
+
+    }
 }

93ff8b84f769eeec9e144f491327faa26a7ccba2
Dominik Mitterer <dmitterer@tsn.at>
2021-11-14 18:43:28 +0100

    Ergänzung in Course: One-To-One Beziehung wurde erstellt inkl. mappedBy für Uni/Bidircetional Relation. Weitere Infos siehe Word Doku Unterpunkt 6.14.1 und 6.14.2 . Erstellen und Implementieren von "CourseRepositoryTest" siehe Word Doku 6.14.1 und 6.14.2 .

--- src/main/java/com/itkollegimst/spring/data/jpa/tutorial/entity/Course.java
+++ src/main/java/com/itkollegimst/spring/data/jpa/tutorial/entity/Course.java
@@ -31,4 +31,12 @@ public class Course {
     private String title;
     private Integer credit;
 
+
+    @OneToOne(
+
+            mappedBy = "course"
+
+    )
+    private CourseMaterial courseMaterial;
+
 }

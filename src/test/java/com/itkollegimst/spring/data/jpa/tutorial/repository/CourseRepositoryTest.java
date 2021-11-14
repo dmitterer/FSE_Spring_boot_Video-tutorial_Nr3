01da605e82e6d403085c75b77ca477ecefad6f33
Dominik Mitterer <dmitterer@tsn.at>
2021-11-14 22:57:34 +0100

    Ergänzung von Pagination  "CourseRepositoryTest" weitere Infos siehe Word Doku Unterpunkt 6.17.1

--- src/test/java/com/itkollegimst/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
+++ src/test/java/com/itkollegimst/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -6,6 +6,8 @@ import com.itkollegimst.spring.data.jpa.tutorial.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -48,5 +50,36 @@ class CourseRepositoryTest {
         courseRepository.save(course);
 
     }
+
+    @Test
+    public void findAllPagination(){
+
+
+
+        Pageable firstPageWithThreeRecords =
+                PageRequest.of(0,3);
+
+        Pageable secondPageWithTwoRecords =
+                PageRequest.of(1,2);
+
+        List<Course> courses = courseRepository.findAll(firstPageWithThreeRecords).getContent();
+
+
+        long totalElements =
+
+                courseRepository.findAll(firstPageWithThreeRecords)
+                .getTotalElements();
+
+        long totalPages =
+                courseRepository.findAll(firstPageWithThreeRecords)
+                .getTotalPages();
+
+        System.out.println("totalPages = " + totalPages);
+        System.out.println("totalElements = " + totalElements);
+        System.out.println("course = " + courses);
+
+
+
+        }
 
 }

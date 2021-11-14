237d8b37fb43640758d0a9f290098c67ecacd91c
Dominik Mitterer <dmitterer@tsn.at>
2021-11-14 17:37:53 +0100

    Ergänzung in CourseMaterial: In der Beziehung wurde noch "fetch = FetchType.LAZY" hinzugefügt. Weitere Infos siehe Word Doku Unterpunkt 6.13.1 Ergänzung der Methode "printAllCourseMaterials" in der Klassen "CourseMaterialRepositoryTest". siehe Word Doku Unterpunkt 6.13.1

--- src/test/java/com/itkollegimst/spring/data/jpa/tutorial/repository/CourseMaterialRepositoryTest.java
+++ src/test/java/com/itkollegimst/spring/data/jpa/tutorial/repository/CourseMaterialRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -35,4 +37,13 @@ class CourseMaterialRepositoryTest {
         repository.save(courseMaterial);
     }
 
+    @Test
+    public void printAllCourseMaterials(){
+        List<CourseMaterial> courseMaterials =
+                repository.findAll();
+
+        System.out.println("courseMaterials = " + courseMaterials);
+
+
+    }
 }
